13dbcce72fc0c6ad0e90b9d447dd417ba66ecb99
Chris Choi <mychoi314@gmail.com>
2023-03-29 23:07:28 -0400

    Implement Causal Mask for the attention layer

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use tch::Tensor;
+use tch::{Tensor, Kind, IndexOp, NewAxis};
 use tch::{nn, nn::Module, Device};
 
 #[derive(Debug)]
@@ -22,18 +22,30 @@ impl nn::Module for NN {
     }
 }
 
+struct Attention {
+    
+}
+
 fn main() {
     let vs = nn::VarStore::new(Device::Cpu);
+    /*
     let model = NN::new(&vs.root());
 
     let input = Tensor::of_slice(&[0f32; 32]);
     // input.print();
     let output = model.forward(&input);
     output.print();
+    */
 
+    let max_positions = 20;
+    let mut causal_mask = Tensor::ones(&[max_positions, max_positions], (Kind::Bool, Device::Cpu));
+    causal_mask = causal_mask.tril(0);
+    causal_mask = causal_mask.i((NewAxis, NewAxis));
+    dbg!(causal_mask.size());
+    causal_mask.print();
     /*
-    let a = Tensor::of_slice(&[3, 1, 4, 1, 5]);
-    let b = Tensor::of_slice(&[3, 1, 4, 1, 5]);
+    let a = Tensor::of_slice(&[3, 1, 4, 1, 5]); [[5,4,4,4,4]] // [1,5]
+    let b = Tensor::of_slice(&[3, 1, 4, 1, 5]); // [5]
     let c = a.unsqueeze(1) * b.unsqueeze(0);
     c.print();
     */
