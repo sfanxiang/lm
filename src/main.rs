5230c627dc1426db81ce133bf12a51a1b02b67af
Chris Choi <mychoi314@gmail.com>
2023-03-30 02:40:44 -0400

    Write attention module

--- src/main.rs
+++ src/main.rs
@@ -1,98 +1,193 @@
-use tch::{Tensor, Kind, IndexOp, NewAxis};
-use tch::{nn, nn::Module, Device};
+use core::borrow::Borrow;
+use tch::{
+    nn,
+    nn::{ModuleT, Path},
+    Device,
+};
+use tch::{IndexOp, Kind, NewAxis, Tensor};
 
 #[derive(Debug)]
-struct NN {
-    model: nn::Sequential,
+struct Dropout {
+    p: f64,
 }
 
-impl NN {
-    pub fn new(vs: &nn::Path) -> Self {
-        let linear = nn::linear(vs / "model", 32, 32, Default::default());
-        linear.bs.as_ref().unwrap().print();
-        Self {
-            model: nn::seq().add(linear),
-        }
+impl Dropout {
+    pub fn new<'a, T: Borrow<Path<'a>>>(_vs: T, p: f64) -> Self {
+        // TODO
+        Self { p }
     }
 }
 
-impl nn::Module for NN {
-    fn forward(&self, xs: &Tensor) -> Tensor {
-        self.model.forward(xs)
+impl nn::ModuleT for Dropout {
+    fn forward_t(&self, xs: &Tensor, train: bool) -> Tensor {
+        xs.dropout(self.p, train)
     }
 }
 
 #[derive(Debug)]
 struct Attention {
     bias: Tensor,
-    masked_bias: Tensor, 
+    masked_bias: Tensor,
     embed_dim: i64,
-    num_heads: i64, 
-    head_dim: i64, 
-    split_size: i64, 
+    num_heads: i64,
+    head_dim: i64,
+    split_size: i64,
     scale_attn_weights: bool,
-    scale_attn_by_inverse_layer_idx: bool, 
+    scale_attn_by_inverse_layer_idx: bool,
     layer_idx: i64,
-    c_attn: nn::Conv1D,
-    q_attn: nn::Conv1D,
-    c_proj: nn::Conv1D,
+    c_attn: nn::Linear,
+    c_proj: nn::Linear,
+    attn_dropout: Dropout,
+    resid_dropout: Dropout,
 }
 
 impl Attention {
-    fn attn(&self, query: &Tensor, key: &Tensor, value: &Tensor, attention_mask: Option<&Tensor>) 
-    -> (Tensor, Tensor){
+    fn new<'a, T: Borrow<Path<'a>>>(vs: T, layer_idx: i64) -> Self {
+        let max_positions = 1024;
+
+        let mut bias = Tensor::ones(&[max_positions, max_positions], (Kind::Bool, Device::Cpu));
+        bias = bias.tril(0);
+        bias = bias.i((NewAxis, NewAxis));
+        let masked_bias = Tensor::full(&[], -1e4, (Kind::Float, Device::Cpu));
+
+        let embed_dim = 768;
+        let num_heads = 12;
+        let head_dim = embed_dim / num_heads;
+        let split_size = embed_dim;
+
+        let scale_attn_weights = true;
+
+        let scale_attn_by_inverse_layer_idx = false;
+        let c_attn = nn::linear(
+            vs.borrow() / "c_attn",
+            embed_dim,
+            embed_dim * 3,
+            Default::default(),
+        ); // TODO
+        let c_proj = nn::linear(
+            vs.borrow() / "c_proj",
+            embed_dim,
+            embed_dim,
+            Default::default(),
+        ); // TODO
+
+        let attn_dropout = Dropout::new(vs.borrow() / "attn_dropout", 0.1);
+        let resid_dropout = Dropout::new(vs.borrow() / "resid_dropout", 0.1);
+
+        Self {
+            bias,
+            masked_bias,
+            embed_dim,
+            num_heads,
+            head_dim,
+            split_size,
+            scale_attn_weights,
+            scale_attn_by_inverse_layer_idx,
+            layer_idx,
+            c_attn,
+            c_proj,
+            attn_dropout,
+            resid_dropout,
+        }
+    }
+
+    fn attn(
+        &self,
+        query: &Tensor,
+        key: &Tensor,
+        value: &Tensor,
+        attention_mask: Option<&Tensor>,
+    ) -> (Tensor, Tensor) {
         let mut attn_weights = query.matmul(&key.transpose(-1, -2));
-        
+
         if self.scale_attn_weights {
-            attn_weights = (&attn_weights) / tch::Tensor::full(&[], (*value.size().last().unwrap() as f64).sqrt(), (attn_weights.kind(), attn_weights.device()));
+            attn_weights = (&attn_weights)
+                / tch::Tensor::full(
+                    &[],
+                    (*value.size().last().unwrap() as f64).sqrt(),
+                    (attn_weights.kind(), attn_weights.device()),
+                );
         }
 
         if self.scale_attn_by_inverse_layer_idx {
-            attn_weights = (&attn_weights) / ((self.layer_idx + 1) as f64) 
+            attn_weights = (&attn_weights) / ((self.layer_idx + 1) as f64)
         }
-        let sec_to_last_query_val = query.size()[query.size().len() - 2];
-        let sec_to_last_key_val = query.size()[key.size().len() - 2];
-        let (query_length, key_length) = (sec_to_last_query_val, sec_to_last_key_val);
-        let causal_mask = self.bias.i((.., .., (key.size().len() - query.size().len()) as i64  .. key.size().len() as i64 , .. key.size().len() as i64 ));
+        let query_length = query.size()[query.size().len() - 2];
+        let key_length = query.size()[key.size().len() - 2];
+        let causal_mask = self.bias.i((
+            ..,
+            ..,
+            key_length - query_length .. key_length,
+            key_length,
+        ));
         let mask_value = -1e10;
-        let mask_value = tch::Tensor::full(&[], mask_value, (attn_weights.kind(), attn_weights.device()));
-        let mut attn_weights = causal_mask.where_self(&attn_weights, &mask_value);
+        let mask_value = tch::Tensor::full(
+            &[],
+            mask_value,
+            (attn_weights.kind(), attn_weights.device()),
+        );
+        let mut attn_weights = attn_weights.where_self(&causal_mask, &mask_value);
 
         if let Some(attention_mask) = attention_mask {
             attn_weights = attn_weights + attention_mask;
         }
         let attn_output = attn_weights.matmul(&value);
 
-        return (attn_output, attn_weights)
+        (attn_output, attn_weights)
     }
-}
 
-impl Module for Attention {
-    fn forward(&self, xs: &Tensor) -> Tensor {
-        todo!()
+    fn split_heads(&self, tensor: &Tensor, num_heads: i64, attn_head_size: i64) -> Tensor {
+        let mut new_shape = tensor.size();
+        new_shape.pop();
+        new_shape.push(num_heads);
+        new_shape.push(attn_head_size);
+        let tensor = tensor.reshape(&new_shape[..]);
+        tensor.permute(&[0, 2, 1, 3]) // (batch, head, seq_length, head_features)
+    }
+
+    fn merge_heads(&self, tensor: &Tensor, num_heads: i64, attn_head_size: i64) -> Tensor {
+        let tensor = tensor.permute(&[0, 2, 1, 3]).contiguous();
+        let mut new_shape = tensor.size();
+        new_shape.pop();
+        new_shape.pop();
+        new_shape.push(num_heads * attn_head_size);
+        tensor.reshape(&new_shape[..])
+    }
+
+    pub fn forward_t(
+        &self,
+        hidden_states: &Tensor,
+        attention_mask: Option<&Tensor>,
+        train: bool,
+    ) -> Tensor {
+        let mut split = self
+            .c_attn
+            .forward_t(hidden_states, train);
+        let mut split = split
+            .split(self.split_size, 2);
+        let mut value = split.pop().unwrap();
+        let mut key = split.pop().unwrap();
+        let mut query = split.pop().unwrap();
+
+        query = self.split_heads(&query, self.num_heads, self.head_dim);
+        key = self.split_heads(&key, self.num_heads, self.head_dim);
+        value = self.split_heads(&value, self.num_heads, self.head_dim);
+
+        let (mut attn_output, attn_weights) = self.attn(&query, &key, &value, attention_mask);
+
+        attn_output = self.merge_heads(&attn_output, self.num_heads, self.head_dim);
+        attn_output = self.c_proj.forward_t(&attn_output, train);
+        attn_output = self.resid_dropout.forward_t(&attn_output, train);
+
+        attn_output
     }
 }
 
 fn main() {
     let vs = nn::VarStore::new(Device::Cpu);
-    /*
-    let model = NN::new(&vs.root());
 
-    let input = Tensor::of_slice(&[0f32; 32]);
-    // input.print();
-    let output = model.forward(&input);
-    output.print();
-    */
-    let max_positions = 20;
-        let mut causal_mask = Tensor::ones(&[max_positions, max_positions], (Kind::Bool, Device::Cpu));
-        causal_mask = causal_mask.tril(0);
-        causal_mask = causal_mask.i((NewAxis, NewAxis));
-        dbg!(causal_mask.size());
-        causal_mask.print();
-    /*
-    let a = Tensor::of_slice(&[3, 1, 4, 1, 5]); [[5,4,4,4,4]] // [1,5]
-    let b = Tensor::of_slice(&[3, 1, 4, 1, 5]); // [5]
-    let c = a.unsqueeze(1) * b.unsqueeze(0);
-    c.print();
-    */
+    let hidden_states = Tensor::zeros(&[1, 8, 768], (Kind::Float, Device::Cpu));
+
+    let attn = Attention::new(&vs.root(), 0);
+    let _output = attn.forward_t(&hidden_states, None, false);
 }
