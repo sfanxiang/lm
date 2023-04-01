f3d8c4aa5ff668528afb834e27e88af83ab2f23c
ChrisChoi314 <mychoi314@gmail.com>
2023-04-01 12:01:19 -0400

    Load the model in afpdsfj.rs

--- src/afpdsfj.rs
+++ src/afpdsfj.rs
@@ -7,17 +7,10 @@ use tch::{nn, Device};
 use super::gpt2;
 
 pub fn afpdsfj() {
-    
-    let weights_path = weights_resource.get_local_path()?;
+    let weights_path = std::path::PathBuf::from("rust_model.ot");
 
     let device = Device::Cpu;
     let mut vs = nn::VarStore::new(device);
-    let tokenizer: Gpt2Tokenizer = Gpt2Tokenizer::from_file(
-        vocab_path.to_str().unwrap(),
-        merges_path.to_str().unwrap(),
-        true,
-    )?;
-    let config = Gpt2Config::from_file(config_path);
-    let gpt2_model = gtp2::GPT2LMHeadModel::new(&vs.root());
-    vs.load(weights_path)?;
+    let gpt2_model = gpt2::GPT2LMHeadModel::new(&vs.root());
+    vs.load(weights_path);
 }
