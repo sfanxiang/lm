c445a60e36e2e988f847a732493e0b3130ae56a6
ChrisChoi314 <mychoi314@gmail.com>
2023-04-03 23:53:45 -0400

    Add greedy decoding

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod gpt2;
 use self::gpt2::GPT2LMHeadModel;
 use clap::Parser;
 use rust_tokenizers::tokenizer::{Gpt2Tokenizer, Tokenizer, TruncationStrategy};
+use std::io::Write;
 use tch::{nn, Device, IndexOp, NewAxis, Tensor};
 
 #[derive(Parser, Debug)]
@@ -15,6 +16,8 @@ struct Args {
     model: String,
     #[clap(long)]
     sentence: String,
+    #[clap(long)]
+    max_new_tokens: i64,
 }
 
 fn main() {
@@ -23,29 +26,37 @@ fn main() {
     let device = Device::Cpu;
 
     let tokenizer = Gpt2Tokenizer::from_file(args.vocab, args.merges, false).unwrap();
-    let input = tokenizer.encode(
-        &args.sentence,
-        None,
-        512,
-        &TruncationStrategy::LongestFirst,
-        0,
-    );
-    let input_ids = Tensor::of_slice(&input.token_ids[..]).i(NewAxis);
 
     let weights_path = std::path::PathBuf::from("rust_model.ot");
     let mut vs = nn::VarStore::new(device);
     let model = GPT2LMHeadModel::new(&vs.root());
     vs.load(weights_path).unwrap();
 
-    println!("Input IDs:");
-    input_ids.print();
-    let output = model.forward_t(&input_ids, None, None, false);
-    let logits = output.logits;
-    let logits = logits.i((.., -1));
-    let token = logits.argmax(Some(-1), false);
+    let mut new_sentence = args.sentence;
+    print!("{}", &new_sentence[13..]);
+    std::io::stdout().flush().unwrap();
+    for _i in 0..args.max_new_tokens {
+        let input = tokenizer.encode(
+            &new_sentence,
+            None,
+            512,
+            &TruncationStrategy::LongestFirst,
+            0,
+        );
+        let input_ids = Tensor::of_slice(&input.token_ids[..]).i(NewAxis);
 
-    let mut token_scalar = [0i64];
-    token.copy_data(&mut token_scalar[..], 1);
-    let token_scalar = token_scalar[0];
-    println!("{}", tokenizer.decode(&[token_scalar], false, false));
+        let output = model.forward_t(&input_ids, None, None, false);
+        let logits = output.logits;
+        let logits = logits.i((.., -1));
+        let token = logits.argmax(Some(-1), false);
+
+        let mut token_scalar = [0i64];
+        token.copy_data(&mut token_scalar[..], 1);
+        let token_scalar = token_scalar[0];
+        let str_to_add = tokenizer.decode(&[token_scalar], false, false);
+        print!("{}", str_to_add);
+        std::io::stdout().flush().unwrap();
+        new_sentence += &str_to_add[..];
+    }
+    print!("{}", "\n");
 }
