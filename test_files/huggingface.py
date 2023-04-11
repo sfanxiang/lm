95f4759c7165b9aae6706fb1c8a9373f648452a8
Xiang Fan <sfanxiang@gmail.com>
2023-04-11 03:28:36 -0400

    Actual, useful, correct tests

--- test_files/huggingface.py
+++ test_files/huggingface.py
@@ -12,7 +12,7 @@ times = []
 for i in range(20):
     start_time = time.time()
     torch.manual_seed(0)
-    result = model.generate(input['input_ids'], do_sample=True, topk = 50, bad_words_ids = [[bad_id]], temperature = 1, max_new_tokens = (i+1)*5)
-    #print(tokenizer.batch_decode(result))
+    result = model.generate(input['input_ids'], do_sample=True, top_k = 50, eos_token_id=[50255], bad_words_ids = [[bad_id]], temperature = 1, max_new_tokens = (i+1)*5)
+    # print(tokenizer.batch_decode(result))
     times += [time.time() - start_time]
 print(times)
